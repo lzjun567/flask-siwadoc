d3b7ad8b67a8f9c823a0168ba79f6d50b20882fc
lzjun <lzjun567@qq.com>
2022-06-06 09:56:08 +0800

    add examples

--- example/__init__.py
+++ example/__init__.py
@@ -19,10 +19,45 @@ def hello():
 
 
 @app.route("/login", methods=["POST"])
-@siwa.doc(body=LoginModel, resp=UserModel)
+@siwa.doc(body=LoginModel, resp=UserModel, tags=['auth'])
 def login(body: LoginModel):
     return {"username": body.username, "id": 1}
 
+
+@app.route("/users/1", methods=["GET"])
+@siwa.doc(resp=UserModel, tags=["user"])
+def users():
+    """
+    user detail
+    """
+    return {"username": "siwa", "id": 1}
+
+
+class QueryModel(BaseModel):
+    gender: str
+
+
+class UpdatePasswordModel(BaseModel):
+    password: str
+
+
+@app.route("/users/<int(min=1):user_id>", methods=["POST"])
+@siwa.doc(query=QueryModel, body=UpdatePasswordModel, resp=UserModel, tags=['auth'])
+def update_password(user_id):
+    """
+    update password
+    """
+    return {"username": "siwa", "id": user_id}
+
+
+#
+@app.route("/users", methods=["GET"])
+@siwa.doc(query=QueryModel, tags=["user"])
+def users_list():
+    """
+    user list
+    """
+    return [{"username": "siwa", "id": 1}]
 
 
 if __name__ == '__main__':
