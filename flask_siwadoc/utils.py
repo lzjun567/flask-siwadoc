36cf65c1d375b0dc8a50ca58230561379a78c820
lzjun <lzjun567@qq.com>
2022-08-08 23:47:41 +0800

    取消对werkzeug指定版本的依赖

--- flask_siwadoc/utils.py
+++ flask_siwadoc/utils.py
@@ -1,10 +1,10 @@
 import inspect
+import re
 from typing import Type, List, Mapping, Any
 
 from pydantic import BaseModel
 from pydantic.typing import Literal
 from werkzeug.datastructures import MultiDict
-from werkzeug.routing import parse_rule, parse_converter_args
 
 from . import schema
 
@@ -24,9 +24,10 @@ def convert_query_params(query_prams: MultiDict, model: Type[BaseModel]) -> dict
 
 def parse_path_params(route: str) -> (str, List):
     """
-    将flask route url 转换成 openapi的path规范
+    解析路径参数
     :param route: /hello/<int(min=2):age>
     :return
+
     path: /hello/{age}
     parameters: [{
                         "in": "path",
@@ -45,7 +46,6 @@ def parse_path_params(route: str) -> (str, List):
     # 会转成两个元素
     # 0:None, None, hello
     # 1:int, min=2, age
-
     for converter, arguments, variable in parse_rule(route):
         if converter is None:
             subs.append(variable)
@@ -123,3 +123,101 @@ def get_path_param_schema(converter: str, *args, **kwargs) -> schema.BaseSchema:
     :param kwargs:
     """
     return schema.SCHEMAS[converter](*args, **kwargs)
+
+
+_rule_re = re.compile(
+    r"""
+    (?P<static>[^<]*)                           # static rule data
+    <
+    (?:
+        (?P<converter>[a-zA-Z_][a-zA-Z0-9_]*)   # converter name
+        (?:\((?P<args>.*?)\))?                  # converter arguments
+        \:                                      # variable delimiter
+    )?
+    (?P<variable>[a-zA-Z_][a-zA-Z0-9_]*)        # variable name
+    >
+    """,
+    re.VERBOSE,
+)
+
+
+def parse_rule(rule):
+    """Parse a rule and return it as generator. Each iteration yields tuples
+    in the form ``(converter, arguments, variable)``. If the converter is
+    `None` it's a static url part, otherwise it's a dynamic one.
+
+    :internal:
+    """
+    pos = 0
+    end = len(rule)
+    do_match = _rule_re.match
+    used_names = set()
+    while pos < end:
+        m = do_match(rule, pos)
+        if m is None:
+            break
+        data = m.groupdict()
+        if data["static"]:
+            yield None, None, data["static"]
+        variable = data["variable"]
+        converter = data["converter"] or "default"
+        if variable in used_names:
+            raise ValueError("variable name %r used twice." % variable)
+        used_names.add(variable)
+        yield converter, data["args"] or None, variable
+        pos = m.end()
+    if pos < end:
+        remaining = rule[pos:]
+        if ">" in remaining or "<" in remaining:
+            raise ValueError("malformed url rule: %r" % rule)
+        yield None, None, remaining
+
+
+def parse_converter_args(argstr):
+    argstr += ","
+    args = []
+    kwargs = {}
+
+    for item in _converter_args_re.finditer(argstr):
+        value = item.group("stringval")
+        if value is None:
+            value = item.group("value")
+        value = _pythonize(value)
+        if not item.group("name"):
+            args.append(value)
+        else:
+            name = item.group("name")
+            kwargs[name] = value
+
+    return tuple(args), kwargs
+
+
+_converter_args_re = re.compile(
+    r"""
+    ((?P<name>\w+)\s*=\s*)?
+    (?P<value>
+        True|False|
+        \d+.\d+|
+        \d+.|
+        \d+|
+        [\w\d_.]+|
+        [urUR]?(?P<stringval>"[^"]*?"|'[^']*')
+    )\s*,
+    """,
+    re.VERBOSE | re.UNICODE,
+)
+
+_PYTHON_CONSTANTS = {"None": None, "True": True, "False": False}
+
+
+def _pythonize(value):
+    if value in _PYTHON_CONSTANTS:
+        return _PYTHON_CONSTANTS[value]
+    for convert in int, float:
+        try:
+            return convert(value)
+        except ValueError:
+            pass
+    if value[:1] == value[-1:] and value[0] in "\"'":
+        value = value[1:-1]
+    return str(value)
